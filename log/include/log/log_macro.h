d9e9f55d12d1796aefa70b7592184ddbb0c671bd
tongjames0923 <tongjames@live.com>
2024-11-26 18:45:10 +0800

    docs(log): 优化日志模块文档注释并扩展功能
    - 为 log.hpp 文件中的类和函数添加详细的文档注释
    - 在 LoggerWrapper 类中添加新的日志级别方法
    - 更新 log_macro.h 文件，增加对不同日志级别的宏定义
    - 优化代码格式和结构，提高可读性

--- log/include/log/log_macro.h
+++ log/include/log/log_macro.h
@@ -2,25 +2,68 @@
 // Created by abstergo on 24-11-12.
 //
 
+// 解除已定义的日志级别宏定义，以防止重复定义错误
 #undef LOG_INFO
 #undef LOG_WARN
 #undef LOG_ERROR
 #undef LOG_DEBUG
 #undef LOG_TRACE
 
+// 包含日志库的头文件
 #include <tbs/log/log.hpp>
 
+// 根据是否定义了 LOGGER_WRAPPER 宏来定义不同的日志级别宏
 #ifndef LOGGER_WRAPPER
+// 如果没有定义 LOGGER_WRAPPER，使用传入的 wrapper 对象进行日志记录
 #define LOG_INFO(wrapper, ...) wrapper.info(LOG_FORMAT(__VA_ARGS__).c_str())
+/// @brief 记录 INFO 级别的日志
+/// @param wrapper 日志记录器对象
+/// @param ... 可变参数列表，用于格式化日志消息
+
 #define LOG_WARN(wrapper, ...) wrapper.warn(LOG_FORMAT(__VA_ARGS__).c_str())
+/// @brief 记录 WARN 级别的日志
+/// @param wrapper 日志记录器对象
+/// @param ... 可变参数列表，用于格式化日志消息
+
 #define LOG_ERROR(wrapper, ...) wrapper.error(LOG_FORMAT(__VA_ARGS__).c_str())
+/// @brief 记录 ERROR 级别的日志
+/// @param wrapper 日志记录器对象
+/// @param ... 可变参数列表，用于格式化日志消息
+
 #define LOG_DEBUG(wrapper, ...) wrapper.debug(LOG_FORMAT(__VA_ARGS__).c_str())
+/// @brief 记录 DEBUG 级别的日志
+/// @param wrapper 日志记录器对象
+/// @param ... 可变参数列表，用于格式化日志消息
+
 #define LOG_TRACE(wrapper, ...) wrapper.trace(LOG_FORMAT(__VA_ARGS__).c_str())
+/// @brief 记录 TRACE 级别的日志
+/// @param wrapper 日志记录器对象
+/// @param ... 可变参数列表，用于格式化日志消息
 
 #else
-#define LOG_INFO( ...) LOGGER_WRAPPER.info(LOG_FORMAT(__VA_ARGS__).c_str())
+// 如果定义了 LOGGER_WRAPPER，使用全局的 LOGGER_WRAPPER 对象进行日志记录
+#define LOG_INFO(...) LOGGER_WRAPPER.info(LOG_FORMAT(__VA_ARGS__).c_str())
+/// @brief 记录 INFO 级别的日志
+/// @param ... 可变参数列表，用于格式化日志消息
+
 #define LOG_WARN(...) LOGGER_WRAPPER.warn(LOG_FORMAT(__VA_ARGS__).c_str())
+/// @brief 记录 WARN 级别的日志
+/// @param ... 可变参数列表，用于格式化日志消息
+
 #define LOG_ERROR(...) LOGGER_WRAPPER.error(LOG_FORMAT(__VA_ARGS__).c_str())
+/// @brief 记录 ERROR 级别的日志
+/// @param ... 可变参数列表，用于格式化日志消息
+
 #define LOG_DEBUG(...) LOGGER_WRAPPER.debug(LOG_FORMAT(__VA_ARGS__).c_str())
+/// @brief 记录 DEBUG 级别的日志
+/// @param ... 可变参数列表，用于格式化日志消息
+
 #define LOG_TRACE(...) LOGGER_WRAPPER.trace(LOG_FORMAT(__VA_ARGS__).c_str())
+/// @brief 记录 TRACE 级别的日志
+/// @param ... 可变参数列表，用于格式化日志消息
+#endif
+
+// 如果定义了 LOGGER_WRAPPER，解除其定义
+#ifdef LOGGER_WRAPPER
+#undef LOGGER_WRAPPER
 #endif
