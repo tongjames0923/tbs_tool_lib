bf6946d6dd4f29d66000e205f120f6370435d76e
tongjames0923 <tongjames@live.com>
2024-11-30 10:54:54 +0800

    feat(base): 重构 PointerToImpl 模板类
    - 引入了重置函数和重新创建函数的概念，增加了模板类的灵活性
    - 实现了默认的重置函数和几种重新创建函数，以适应不同的需求
    - 重构了构造函数、赋值运算符等成员函数，提高了代码的可读性和可维护性
    - 添加了 static_assert 断言，确保重置函数和重新创建函数不为 nullptr
    - 优化了资源管理，确保在各种情况下都能正确释放资源

--- base/include/tbs/PointerToImpl.h
+++ base/include/tbs/PointerToImpl.h
@@ -1,68 +1,133 @@
-//
-// Created by tongj on 24-11-20.
-//
-
 #ifndef POINTERTOIMPL_H
 #define POINTERTOIMPL_H
-
-#pragma GCC diagnostic ignored "-Wdelete-incomplete"
-#pragma warning(push)
-#pragma warning(disable : 4150)
-
-
-#include <tbs/defs.h>
 #include <memory>
+#include <tbs/defs.h>
 
-class Resetor;
+    /**
+     * @brief 定义了一个重置函数的类型别名。
+     *
+     * @tparam T 实现类的类型。
+     */
+    template <typename T>
+    using __resetor = void(T*&);
 
 /**
- * 模板类PointerImpl实现了指向实现的指针，用于封装实际的实现细节。
+ * @brief 默认的重置函数，用于删除指针并将其设置为 nullptr。
+ *
+ * @tparam T 实现类的类型。
+ * @param ptr 需要重置的指针。
+ */
+template <typename T>
+void default_resetor(T*& ptr)
+{
+    if (ptr == nullptr)
+    {
+        return;
+    }
+    delete ptr;
+    ptr = nullptr;
+}
+
+/**
+ * @brief 定义了一个重新创建对象的函数类型别名。
+ *
+ * @tparam T 实现类的类型。
+ */
+template <typename T>
+using __remake_func = T*();
+
+/**
+ * @brief 默认的重新创建函数，抛出运行时错误表示无法重新创建指针。
+ *
+ * @tparam T 实现类的类型。
+ * @throws std::runtime_error 抛出运行时错误，表示无法重新创建指针。
+ */
+template <typename T>
+T* no_remake()
+{
+    throw std::runtime_error("cannot remake pointer");
+}
+
+/**
+ * @brief 默认的重新创建函数，创建一个新的实现类对象。
+ *
+ * @tparam T 实现类的类型。
+ * @return 返回新创建的实现类对象的指针。
+ */
+template <typename T>
+T* empty_args_remake()
+{
+    return new T();
+}
+
+/**
+ * @brief 模板类 PointerImpl 实现了指向实现的指针，用于封装实际的实现细节。
+ *
  * 这种模式通常用于隐藏实际的实现细节，提供一种更清晰的接口。
  *
  * @tparam IMPL 实际实现的类类型。
+ * @tparam _resetor 重置函数，默认为 default_resetor。
+ * @tparam _remake 重新创建函数，默认为 no_remake。
  */
-template <typename IMPL, typename R = Resetor>
+template <typename IMPL, __resetor<IMPL> _resetor = default_resetor<IMPL>, __remake_func<IMPL> _remake = no_remake<IMPL>>
 class PointerImpl
 {
 private:
     using IMPL_PTR = IMPL*;
     // 封装实现类的智能指针，确保资源的正确管理。
-    IMPL_PTR m_impl = nullptr;
-    R m_reset;
+    mutable IMPL_PTR m_impl = nullptr;
+
+    static_assert(_resetor != nullptr, "PointerImpl::PointerImpl: cannot reset pointer");
+    static_assert(_remake != nullptr, "PointerImpl::PointerImpl: cannot remake pointer");
+
+    /**
+     * @brief 如果 m_impl 为 nullptr，则调用 _remake 函数重新创建对象。
+     */
+    void ifNullImpl() const
+    {
+        if (m_impl == nullptr)
+        {
+            m_impl = _remake();
+        }
+    }
 
 protected:
     /**
-     * 获取实现类的常引用。
+     * @brief 获取实现类的常引用。
      *
      * @return 实现类的常引用。
      */
-    CONST IMPL& getImpl() const
+    const IMPL& getImpl() const
     {
-        ASSERT(m_impl == nullptr, "PointerImpl::getImpl: nullptr pointer")
+        ifNullImpl();
         return *m_impl;
     }
 
     /**
-     * 获取实现类的引用。
+     * @brief 获取实现类的引用。
      *
      * @return 实现类的引用。
      */
     IMPL& getImpl()
     {
-        ASSERT(m_impl == nullptr, "PointerImpl::getImpl: nullptr pointer")
+        ifNullImpl();
         return *m_impl;
     }
 
+    /**
+     * @brief 重置实现类的指针。
+     *
+     * @param p 指向新实现类的指针。可以为 nullptr。
+     */
     void reset(IMPL* p)
     {
-
         if (p == this->m_impl)
         {
             return;
         }
         if (m_impl != nullptr)
         {
-            m_reset.operator()(m_impl);
+            _resetor(m_impl);
         }
 
         m_impl = p;
@@ -70,9 +135,9 @@ protected:
 
 public:
     /**
-     * 构造函数，初始化实现类的指针。
+     * @brief 构造函数，初始化实现类的指针。
      *
-     * @param impl_pointer 指向实现类的指针，默认为new IMPL()。
+     * @param impl_pointer 指向实现类的指针，默认为 new IMPL()。
      */
     explicit PointerImpl(IMPL* impl_pointer = new IMPL())
     {
@@ -80,86 +145,95 @@ public:
     }
 
     /**
-     * 拷贝构造函数，仅当实现类可拷贝构造时有效。
+     * @brief 模板构造函数，用于创建指向指定实现类的指针。
      *
-     * @param other 另一个PointerImpl对象。
+     * @tparam _Args 构造函数参数类型。
+     * @param args 构造函数参数。
      */
-    template <typename T = IMPL>
-    explicit PointerImpl(std::enable_if_t<std::is_copy_constructible_v<T>, const PointerImpl&> other)
+    template <typename... _Args>
+    explicit PointerImpl(_Args&&... args)
     {
-        ASSERT(other.m_impl == nullptr, "PointerImpl::PointerImpl: nullptr pointer");
-        if (other.m_impl != this->m_impl)
-        {
-            reset(new T(*other.m_impl));
-        }
+        reset(new IMPL(std::forward<_Args>(args)...));
     }
 
-    explicit PointerImpl(PointerImpl&& other) noexcept
+    /**
+     * @brief 从另一个 PointerImpl 对象复制数据。
+     *
+     * @param other 要复制的 PointerImpl 对象。
+     */
+    void copyFrom(const PointerImpl& other)
     {
-        ASSERT(other.m_impl == nullptr, "PointerImpl::PointerImpl: nullptr pointer");
         if (other.m_impl != this->m_impl)
         {
-            reset(other.m_impl);
+            if constexpr (std::is_copy_constructible_v<IMPL>)
+            {
+                reset(new IMPL(other.getImpl()));
+            }
+            else if constexpr (std::is_copy_assignable_v<IMPL>)
+            {
+                getImpl() = other.getImpl();
+            }
+            else
+            {
+                throw std::runtime_error("PointerImpl::PointerImpl: cannot copy from other pointer");
+            }
         }
     }
 
     /**
-     * 拷贝赋值运算符，仅当实现类可拷贝赋值时有效。
+     * @brief 从另一个 PointerImpl 对象移动数据。
      *
-     * @param other 另一个PointerImpl对象。
-     * @return 返回当前PointerImpl对象的引用。
+     * @param other 要移动的 PointerImpl 对象。
      */
-    template <typename T = IMPL>
-    PointerImpl& operator=(std::enable_if_t<std::is_copy_assignable_v<T>, const PointerImpl&> other)
+    void moveFrom(PointerImpl&& other) noexcept
     {
-        ASSERT(other.m_impl == nullptr, "PointerImpl::PointerImpl: nullptr pointer");
-        if (other.m_impl != this->m_impl)
-        {
-            T* ptr = new T();
-            *ptr = *other.m_impl;
-            reset(ptr);
-        }
-
-        return *this;
-    }
-
-    /**
-     * 移动赋值运算符，仅当实现类可移动赋值时有效。
-     *
-     * @param other 另一个要移动的PointerImpl对象。
-     * @return 返回当前PointerImpl对象的引用。
-     */
-    template <typename T = IMPL>
-    PointerImpl& operator=(PointerImpl&& other) noexcept
-    {
-        ASSERT(other.m_impl == nullptr, "PointerImpl::PointerImpl: nullptr pointer");
-        if (other.m_impl != this->m_impl)
-        {
-            reset(other.m_impl);
-        }
-
-        return *this;
-    }
-
-    virtual ~PointerImpl()
-    {
-        reset(nullptr);
-        }
-};
-
-class Resetor
-{
-public:
-    template<typename T>
-    void operator()(T *&ptr)
-    {
-        if (ptr==nullptr)
+        if (other.m_impl == nullptr)
         {
             return;
         }
-        delete ptr;
-        ptr = nullptr;
+        reset(other.m_impl);
+        other.m_impl = nullptr;
+    }
+
+    /**
+     * @brief 删除拷贝赋值操作符。
+     */
+    DELETE_COPY_ASSIGNMENT(PointerImpl)
+
+    /**
+     * @brief 删除拷贝构造函数。
+     */
+    DELETE_COPY_CONSTRUCTION(PointerImpl)
+
+    /**
+     * @brief 移动构造函数。
+     *
+     * @param other 要移动的 PointerImpl 对象。
+     */
+    PointerImpl(PointerImpl&& other) noexcept
+    {
+        moveFrom(std::move(other));
+    }
+
+    /**
+     * @brief 移动赋值操作符。
+     *
+     * @param other 要移动的 PointerImpl 对象。
+     * @return 返回当前对象的引用。
+     */
+    PointerImpl& operator=(PointerImpl&& other) noexcept
+    {
+        moveFrom(std::move(other));
+        return *this;
+    }
+
+    /**
+     * @brief 析构函数，释放资源。
+     */
+    virtual ~PointerImpl()
+    {
+        reset(nullptr);
     }
 };
 
-#endif //POINTERTOIMPL_H
+#endif // POINTERTOIMPL_H
