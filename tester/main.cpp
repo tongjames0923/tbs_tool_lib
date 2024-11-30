ecae4052e923df018bf6a05ee7cad7124d779eea
tongjames0923 <tongjames@live.com>
2024-11-30 21:05:18 +0800

    perf(tester): 使用循环队列优化性能
    - 新增 FixedQueue 类实现循环队列
    - 使用 ConstexprString64 替代直接字符串操作
    - 添加时间测量，评估性能改进
    - 移除未使用的编译时条件判断代码

--- tester/main.cpp
+++ tester/main.cpp
@@ -1,23 +1,96 @@
 #include <array>
 #include <iostream>
 #include <optional>
+#include <queue>
 #include <string>
+#include <vector>
 
 #include <tbs/compile_time_function.h>
 #include <tbs/constexpr_value.h>
+#include <tbs/time_utils.hpp>
 
+#include "../base/include/tbs/constexpr_value.h"
+
+template <typename T, size_t N>
+class FixedQueue
+{
+private:
+    std::array<T, N> m_data;
+    size_t m_tail = 0, m_head = 0;
+
+public:
+    FixedQueue() = default;
+    FixedQueue(const FixedQueue&) = default;
+    FixedQueue& operator=(const FixedQueue&) = default;
+    FixedQueue(FixedQueue&&) = default;
+    FixedQueue& operator=(FixedQueue&&) = default;
+    FixedQueue(std::initializer_list<T> list)
+    {
+        for (auto& elem : list)
+        {
+            push(elem);
+        }
+    }
+    bool empty() const
+    {
+        return m_head == m_tail;
+    }
+    bool full() const
+    {
+        return m_head == (m_tail + 1) % N;
+    }
+    void push(const T& elem)
+    {
+        if (full())
+        {
+            return;
+        }
+        m_data[m_tail] = elem;
+        m_tail = (m_tail + 1) % N;
+    }
+    void pop()
+    {
+        if (empty())
+        {
+            throw std::runtime_error("empty queue");
+        }
+        m_head = (m_head + 1) % N;
+    }
+    CONST T& front() const
+    {
+        if (empty())
+        {
+            throw std::runtime_error("empty queue");
+        }
+        return m_data[m_head];
+    }
+    T& front()
+    {
+        if (empty())
+        {
+            throw std::runtime_error("empty queue");
+        }
+        return m_data[m_head];
+    }
+};
+
+using DefaultString = ConstexprString64;
 int main()
 {
-    using namespace std;
-    constexpr auto i = COMPILE_TIME_IF_RESULT(make_ConstexprValue({1, 2, 3, 4}) == make_ConstexprValue({1, 2, 5, 4}), []() { return; }, []() { return 68; });
-    if constexpr (i.has_value())
+    auto now = time_utils::utils_now();
+    bool b1 = true;
+    for (int i = 0; i < 1000 * 10000; i++)
     {
-        cout << i.value() << endl;
+        constexpr auto a = DefaultString("hello world");
+        constexpr auto b = DefaultString(" are you ok?");
+        constexpr auto c = a + b;
+        // if (b1)
+        // {
+        //     std::cout << c.c_str() << std::endl;
+        //     b1 = false;
+        // }
     }
-    else
-    {
-        cout << "no value" << endl;
-    }
+    std::cout << time_utils::utils_now() - now << std::endl;
 
     return 0;
 }
