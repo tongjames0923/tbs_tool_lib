e0b195189055cd457509c546ecf3b910af8012d3
tongjames0923 <tongjames@live.com>
2024-12-20 19:24:44 +0800

    feat(tester): 添加 Option 类型测试
    - 在 main.cpp 中添加了对 tbs::Option 类型的测试
    - 实现了一个简单的 test 函数，返回一个 Option<int> 类型的值
    - 使用 match 和 matchWithoutReturn 宏对 Option 类型进行模式匹配测试- 测试了 SOME_OPTION 宏的使用

--- tester/main.cpp
+++ tester/main.cpp
@@ -5,25 +5,28 @@
 #include <string>
 #include <vector>
 
+#include <tbs/Option.h>
 #include <tbs/compile_time_function.h>
 #include <tbs/constexpr_value.h>
 #include <tbs/containers/CircleQueue.h>
 #include <tbs/containers/iterator/Iteratable.h>
+#include <tbs/match/match_macro.h>
 #include <tbs/time_utils.hpp>
+
+Option<int> test()
+{
+    return SOME_OPTION(3);
+}
 
 
 int main()
 {
-    CircleQueue<int, 3> c;
-    c.push(1);
-    c.push(2);
-    c.push(3);
-    c.pop();
-    c.push(4);
-    for (auto& elem : c)
-    {
-        std::cout << elem << std::endl;
-    }
 
+    auto k = match(test(), EQUAL_MATCH(SOME_OPTION(3), return SOME_OPTION(8);), []() { return SOME_OPTION(6); });
+    matchWithoutReturn(test(),
+                       EQUAL_MATCH(SOME_OPTION(3), std::cout << "3" << std::endl;),
+                       EQUAL_MATCH(SOME_OPTION(4), std::cout << "4" << std::endl;),
+                       []() { std::cout << "none" << std::endl; });
+    NO_RETURNED_MATCH(test(), EQUAL_MATCH(SOME_OPTION(3), std::cout << "3" << std::endl;), );
     return 0;
 }
